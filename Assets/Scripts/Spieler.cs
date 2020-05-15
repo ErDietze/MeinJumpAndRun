41fea3895575f4d216e2b11ee449784f1a62212e
ErDietze <eric-dietze@web.de>
2020-05-15 23:45:11 +0200

    next first steps

---
+++ Assets/Scripts/Spieler.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class Spieler : MonoBehaviour
+{
+    float eingabeFaktor = 10;
+
+    // Update is called once per frame
+    void Update()
+    {
+        /* Eingabe speichern */
+        float xEingabe = Input.GetAxis("Horizontal");
+        float yEingabe = Input.GetAxis("Vertical");
+
+        // neue Position bestimmen
+        float xNeu = transform.position.x + xEingabe * eingabeFaktor * Time.deltaTime;
+        float yNeu = transform.position.y + yEingabe + eingabeFaktor * Time.deltaTime;
+        transform.position = new Vector3(xNeu, yNeu, 0);
+    }
+}
